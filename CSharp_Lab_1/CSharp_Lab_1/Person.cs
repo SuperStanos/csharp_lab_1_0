e17e2d4b56d3fbcfb7093459853a17bf66990a50
SuperStanos <ryangriebel@gmail.com>
2013-09-03 15:03:45 -0500

    Dog Races
    Work in progress.

---
+++ CSharp_Lab_1/CSharp_Lab_1/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DogBetTest
+{
+    class Person
+    {
+        public string Name { get; set; }
+
+        public int Cash { get; set; }
+    }
+}
